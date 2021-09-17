7dc75627a7338e31e9742a3edf68dfad46339408
Izzat Rosdin <izzat.rosdin@gmail.com>
2021-09-17 17:42:07 +0800

    update: migration

---
+++ database/migrations/2021_09_17_012719_create_personnels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonnelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 155);
+            $table->string('designation', 155);
+            $table->tinyInteger('role')->comment('1-maker,2-checker,3-approver');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnels');
+    }
+}
---
+++ database/migrations/2021_09_17_015344_create_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('ref_no',50)->nullable()->comment('ref no');
+            $table->string('filename')->nullable();
+            $table->string('path')->nullable();
+            $table->integer('created_by')->default(1);
+            $table->integer('updated_by')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
