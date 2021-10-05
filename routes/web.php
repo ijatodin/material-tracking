29617b4c3333ffd665b48836574fb6b7df7b7bf8
Izzat Rosdin <izzat.rosdin@gmail.com>
2021-10-05 15:24:26 +0800

    wip: fixing route

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
