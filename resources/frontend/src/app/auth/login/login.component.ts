49928da47e4687b98fcf40f0fdea3c4b5802cd72
Izzat Rosdin <izzat.rosdin@gmail.com>
2021-10-06 17:49:52 +0800

    fix: logout issue and error handling

--- resources/frontend/src/app/auth/login/login.component.ts
+++ resources/frontend/src/app/auth/login/login.component.ts
@@ -11,6 +11,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 export class LoginComponent implements OnInit {
 
   loginForm: any = {};
+  errorMessage: string = null;
 
   constructor(
     private authSvc: AuthenticationService,
@@ -23,8 +24,11 @@ export class LoginComponent implements OnInit {
   login() {
     console.log(this.loginForm);
     this.authSvc.login(this.loginForm.email, this.loginForm.password).pipe(first()).subscribe((data) => {
+      console.log(data);
       this.router.navigate(["/receiving"]);
-    })
+    },() => {
+      this.errorMessage = 'The provided credentials are incorrect.';
+    });
   }
 
 }
