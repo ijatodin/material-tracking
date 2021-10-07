52352e8b0249fd6bb0a6216182eb03d3dc23d4f7
Izzat Rosdin <izzat.rosdin@gmail.com>
2021-10-07 11:32:11 +0800

    update: added registry seeder fix bug #8

---
+++ database/seeders/RegistryCounterSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\registryCounters;
+use Illuminate\Database\Seeder;
+
+class RegistryCounterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $rcv = registryCounters::firstOrCreate(
+            ['prefix' => 'RCV'],
+            [
+                'counter' => 0,
+                'description' => 'RECEIVING',
+            ]
+        );
+
+        if ($rcv) {
+            echo 'DONE ' . $rcv->prefix . '->' . $rcv->description.PHP_EOL;
+        }
+
+        $smry = registryCounters::firstOrCreate(
+            ['prefix' => 'SMRY'],
+            [
+                'counter' => 0,
+                'description' => 'SUMMARY',
+            ]
+        );
+
+        if ($smry) {
+            echo 'DONE ' . $smry->prefix . '->' . $smry->description.PHP_EOL;
+        }
+    }
+}
